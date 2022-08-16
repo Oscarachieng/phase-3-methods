fbf001665ad32147ec344a331a98dd587c73b00f
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-16 16:50:29 +0300

    Done.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+# greet with default methods
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+greet_with_default 
+
+#add
+def add num1,num2
+    num1 + num2
+end
+sum = add 20,30
+puts sum
+
+#halve
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+    num / 2
+end
+ half = halve 90
+ puts half
